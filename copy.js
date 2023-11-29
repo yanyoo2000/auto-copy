fb7c81aea39965e77f93c9628116164bd636777e
yanyoo2000 <1784135426@qq.com>
2023-11-29 14:56:18 +0800

    增加对复制文件非字符串的异常兼容

--- copy.js
+++ copy.js
@@ -2,14 +2,17 @@ import clipboardy from "clipboardy";
 import fs from "fs";
 
 let lastClipboardContent = clipboardy.readSync();
-let count = 1
+let times = 1;
+let isClipboardContentString = true;
+
 // 定义一个函数用于保存剪切板内容到txt文件
 const saveClipboardToTxt = (text) => {
   try {
     fs.writeFileSync("D:\\clipboard.txt", text + "\n");
-    console.log(count + ". 剪切板内容已保存到clipboard.txt文件中: ");
-    console.log(text.slice(0,50) + ' ......');
-    count++
+    console.log(times + ". 剪切板内容已保存到clipboard.txt文件中: ");
+    isClipboardContentString = true;
+    console.log(text.slice(0, 50) + " ......");
+    times++;
   } catch (err) {
     console.error("保存文件时出现错误: " + err);
   }
@@ -17,17 +20,25 @@ const saveClipboardToTxt = (text) => {
 
 // 定时检查剪切板内容变化并保存到txt文件
 const intervalId = setInterval(() => {
-  const text = clipboardy.readSync();
-  if (text !== lastClipboardContent) {
-    saveClipboardToTxt(text);
-    lastClipboardContent = text;
+  try {
+    const text = clipboardy.readSync();
+    if (text !== lastClipboardContent) {
+      saveClipboardToTxt(text);
+      lastClipboardContent = text;
+    }
+  } catch (error) {
+    if (!isClipboardContentString) {
+      return;
+    }
+    console.log("跳过...当前剪切板的内容不是string ");
+    isClipboardContentString = false;
   }
 }, 1000); // 每秒检查一次
 
 console.log("正在监听剪切板内容…");
 
 // 程序结束时清理定时器
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   clearInterval(intervalId);
   console.log("程序结束，清理定时器");
   process.exit();
--- paste.js
+++ paste.js
@@ -1,19 +1,19 @@
 import fs from "fs";
 import clipboardy from "clipboardy";
 
-let targetFile = "\\\\Client\\D$\\clipboard.txt"
+let targetFile = "\\\\Client\\D$\\clipboard.txt";
 
 let lastFileContent = fs.readFileSync(targetFile, "utf8");
-let count = 0
+let times = 0;
 
 // 定义一个函数用于保存剪切板内容到txt文件
 const writeTextToClipboard = () => {
   try {
     const newFileContent = fs.readFileSync(targetFile, "utf8");
-    if (newFileContent !== lastFileContent || count===0) {
+    if (newFileContent !== lastFileContent || times === 0) {
       clipboardy.writeSync(newFileContent);
-      console.log(count + ". 文件内容已复制到剪切板");
-      count++
+      console.log(times + ". 文件内容已复制到剪切板");
+      times++;
       lastFileContent = newFileContent;
     }
   } catch (err) {
@@ -21,16 +21,16 @@ const writeTextToClipboard = () => {
   }
 };
 
-writeTextToClipboard()
+writeTextToClipboard();
 // 定时检查文件内容变化并复制到剪贴板
 const intervalId = setInterval(() => {
-  writeTextToClipboard()
+  writeTextToClipboard();
 }, 1000); // 每秒检查一次
 
 console.log("正在监听文件内容变化...");
 
 // 程序结束时清理定时器
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   clearInterval(intervalId);
   console.log("程序结束，清理定时器");
   process.exit();
